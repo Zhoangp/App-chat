8f9e08ecd4c3fba0057e7325e23f1cae2105611a
Nguyễn Hoàng Phúc <nguyenhoangphuc@MacBook-Pro-cua-Nguyen.local>
2021-12-17 10:39:54 +0700

    1

--- src/views/chat/Chat.js
+++ src/views/chat/Chat.js
@@ -1,58 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import queryString from 'query-string'
-import io from 'socket.io-client'
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import { useLocation } from "react-router-dom";
 
-let socket
-let infor
+let socket;
+let infor;
 const Chat = (props) => {
-    const [message, setMessage] = useState("");
-    const [listMessage, setListMessage] = useState([])
-    const location = useLocation()
-    const ENDPOINT = 'https://watch-app-chat.herokuapp.com/'
-    useEffect(() => {
-        infor = queryString.parse(location.search)
-        socket = io(ENDPOINT, { transports : ['websocket'] });
-        socket.emit('join', infor)
-    }, [location.search])
-    useEffect(() => {
-        socket.on('receiveMess', (data) => {
-            setListMessage([...listMessage, data])
-            console.log(listMessage)
+  const [message, setMessage] = useState("");
+  const [listMessage, setListMessage] = useState([]);
+  const location = useLocation();
+  const ENDPOINT = "localhost:5000"; /* https://watch-app-chat.herokuapp.com/ */
+  useEffect(() => {
+    infor = queryString.parse(location.search);
+    socket = io(ENDPOINT, { transports: ["websocket"] });
+    socket.emit("join", infor);
+  }, [location.search]);
 
-        })
-    }, [listMessage])
-    const send = async () => {
-            if(message !== "") {
-                const data = {
-                    mess: message,
-                    user: infor.name,
-                    room: infor.room,
-                    time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
-                }
-                
-                await socket.emit('send_message', data)
-                setMessage("")
-            }
+  useEffect(() => {
+    socket.on("receiveMess", (data) => {
+        setListMessage((list) => [...list, data]);
+        console.log(listMessage);
+    });
+  }, [socket]);
+
+  const send = async () => {
+    if (message !== "") {
+      const data = {
+        mess: message,
+        user: infor.name,
+        room: infor.room,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      setListMessage((list) => [...list, data]);
+      await socket.emit("send_message", data);
+      setMessage("");
     }
-    return (
-        <div>
-            <h1>Chat</h1>
-            <div>
-                {listMessage.map(item => {
-                    if(item.user !== queryString.parse(location.search).name)
-                        return <p>{item.mess}</p>
-                    return <p style={{textAlign: 'right', width: '15%'}}>{item.mess}</p>
-                })}
-                <input value={message} type="text" placeholder="Something" onKeyPress={event => {
-                    event.key === 'Enter' && send()
-                }} onChange={(event) => {
-                    setMessage(event.target.value)
-                }}/>
-                <button onClick={send}>Send</button>
-            </div>
-        </div>
-    );
+  };
+  return (
+    <div>
+      <h1>Chat</h1>
+      <div>
+        {listMessage.map((item, index) => {
+          if (item.user !== queryString.parse(location.search).name)
+            return <p key={index}>{item.mess}</p>;
+          return (
+            <p style={{ textAlign: "right", width: "15%" }}>{item.mess}</p>
+          );
+        })}
+        <input
+          value={message}
+          type="text"
+          placeholder="Something"
+          onKeyPress={(event) => {
+            event.key === "Enter" && send();
+          }}
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        />
+        <button onClick={send}>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
